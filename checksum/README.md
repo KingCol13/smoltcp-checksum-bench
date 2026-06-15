aba621203fef7b704745730f86b8bbc807f9be49
KingCol13 <48412633+KingCol13@users.noreply.github.com>
2026-06-15 19:29:52 +0100

    Add criterion benchmarks

---
+++ checksum/README.md
@@ -0,0 +1,9 @@
+# checksum
+
+Variety of implementations of internet checksum
+
+## Criterion benchmarks
+
+```bash
+RUSTFLAGS="-C target-cpu=native" cargo bench
+```
---
+++ checksum/benches/checksum.rs
@@ -0,0 +1,31 @@
+use std::hint::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let data = vec![254u8; 4096];
+
+    c.bench_function("original 64", |b| b.iter(|| checksum::checksum_original(black_box(&data[..64]))));
+    c.bench_function("original 1024", |b| b.iter(|| checksum::checksum_original(black_box(&data[..1024]))));
+
+    c.bench_function("indexed 64", |b| b.iter(|| checksum::checksum_indexed(black_box(&data[..64]))));
+    c.bench_function("indexed 1024", |b| b.iter(|| checksum::checksum_indexed(black_box(&data[..1024]))));
+
+    c.bench_function("chunks_exact 64", |b| b.iter(|| checksum::checksum_chunks_exact(black_box(&data[..64]))));
+    c.bench_function("chunks_exact 1024", |b| b.iter(|| checksum::checksum_chunks_exact(black_box(&data[..1024]))));
+
+    c.bench_function("chunks_exact_no_bigchunk 64", |b| b.iter(|| checksum::checksum_chunks_exact_no_bigchunk(black_box(&data[..64]))));
+    c.bench_function("chunks_exact_no_bigchunk 1024", |b| b.iter(|| checksum::checksum_chunks_exact_no_bigchunk(black_box(&data[..1024]))));
+
+    c.bench_function("sliced_ne 64", |b| b.iter(|| checksum::checksum_sliced_ne(black_box(&data[..64]))));
+    c.bench_function("sliced_ne 2048", |b| b.iter(|| checksum::checksum_sliced_ne(black_box(&data[..2048]))));
+
+    c.bench_function("sliced_ne_u16 16", |b| b.iter(|| checksum::checksum_sliced_ne_u16(black_box(&data[..16]))));
+    c.bench_function("sliced_ne_u16 2048", |b| b.iter(|| checksum::checksum_sliced_ne_u16(black_box(&data[..2048]))));
+
+    c.bench_function("chunks_ne_u16 16", |b| b.iter(|| checksum::checksum_chunks_ne_u16(black_box(&data[..16]))));
+    c.bench_function("chunks_ne_u16 2048", |b| b.iter(|| checksum::checksum_chunks_ne_u16(black_box(&data[..2048]))));
+
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
